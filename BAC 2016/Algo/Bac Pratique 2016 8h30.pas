b8cb3dc2bbba0d2716f2c0c709b081f8dd2dddd5
Jevro1337 <malekamir56@gmail.com>
2021-05-09 23:19:42 +0100

    Added code formatting for Bac Pratique 2016 8h30

--- BAC 2016/Algo/Bac Pratique 2016 8h30.pas
+++ BAC 2016/Algo/Bac Pratique 2016 8h30.pas
@@ -30,15 +30,18 @@ Begin
 End;
 Procedure PermierFactoriel(x:Longint; Var foo:Boolean; Var y:Longint; Var s:Longint);
 Begin
-  foo := False;      // Test init à false
+  foo := False;
+  // Test init à false
   s := 0;
-  y := 0;	
-  If (Premier(Fact(x) + 1)) Then // Si (factoriel de x) + 1 est un nombre premier alors le stocker dans Y et test:= true
+  y := 0;
+  If (Premier(Fact(x) + 1)) Then
+    // Si (factoriel de x) + 1 est un nombre premier alors le stocker dans Y et test:= true
     Begin
-      foo := True;      
+      foo := True;
       y := Fact(x) + 1
     End;
-  If (Premier(Fact(x) - 1)) Then // Si (factoriel de x) - 1 est un nombre premier alors le stocker dans S et test:= true
+  If (Premier(Fact(x) - 1)) Then
+    // Si (factoriel de x) - 1 est un nombre premier alors le stocker dans S et test:= true
     Begin
       foo := True;
       s := Fact(x) - 1;
@@ -48,19 +51,21 @@ Procedure PremierPrimoriel(x:Longint; Var foo:Boolean;Var z:Longint; Var y:Longi
 Var
   i,s: Longint;
 Begin
-  foo := False;        // Test init à false
+  foo := False;
+  // Test init à false
   s := 1;
   y := 0;
   z := 0;
   For i:= 1 To x Do
     If Premier(i) Then
-      s := s*i;                 // Stocker la somme des nombres premiers inferieurs à x dans S
-  If Premier(s+1) Then         // Si S+1 est premier alors stocker S+1 dans z et test:= true
+      s := s*i;
+  // Stocker la somme des nombres premiers inferieurs à x dans S
+  If Premier(s+1) Then // Si S+1 est premier alors stocker S+1 dans z et test:= true
     Begin
       foo := True;
       z := s+1
     End;
-  If premier(s-1) Then       // Si S-1 est premier alors stocker S-1 dans y et test:= true
+  If premier(s-1) Then // Si S-1 est premier alors stocker S-1 dans y et test:= true
     Begin
       foo := True;
       y := s-1;
@@ -75,21 +80,23 @@ Begin
   j := 0;
   Repeat
     i := i+1;
-        PermierFactoriel(i,test,s,y);
-        If test Then                   
+    PermierFactoriel(i,test,s,y);
+    If test Then
+      Begin
+        If Y<>0 Then //Si (factoriel de x) - 1 est premier alors l'afficher et incrémenter j
           Begin
-            If Y<>0 Then                   //Si (factoriel de x) - 1 est premier alors l'afficher et incrémenter j
-              Begin
-                Writeln(Y,' est un Premier Factoriel');
-                j := j+1;
-              End;
-            If (S<>0) and (j<>n) Then   //Si (factoriel de x) + 1 est premier alors l'afficher et incrémenter j (et j <> n pour eviter le double incrementation)
-              Begin
-                Writeln(S,' est un Premier Factoriel');
-                j := j+1;
-              End;
+            Writeln(Y,' est un Premier Factoriel');
+            j := j+1;
           End;
-  Until j=n;                                                                                      
+        If (S<>0) And (j<>n) Then
+
+//Si (factoriel de x) + 1 est premier alors l'afficher et incrémenter j (et j <> n pour eviter le double incrementation)
+          Begin
+            Writeln(S,' est un Premier Factoriel');
+            j := j+1;
+          End;
+      End;
+  Until j=n;
   i := 1;
   j := 0;
   Repeat
@@ -99,18 +106,21 @@ Begin
         PremierPrimoriel(i,test,s,y);
         If test Then
           Begin
-            If Y<>0 Then       //Si (la somme des nombres premiers inferieurs à N) - 1 est premier alors l'afficher et incrémenter j
+            If Y<>0 Then
+
+//Si (la somme des nombres premiers inferieurs à N) - 1 est premier alors l'afficher et incrémenter j
               Begin
-                Writeln(Y,' est un Premier Primoriel');     
+                Writeln(Y,' est un Premier Primoriel');
                 j := j+1;
               End;
-            If (S<>0) and (j<>n) Then //Si (la somme des nombres premiers inferieurs à N) + 1 est premier alors l'afficher et incrémenter j  (et j <> n pour eviter le double incrementation)
+            If (S<>0) And (j<>n) Then
+
+//Si (la somme des nombres premiers inferieurs à N) + 1 est premier alors l'afficher et incrémenter j  (et j <> n pour eviter le double incrementation)
               Begin
                 Writeln(S,' est un Premier Primoriel');
                 j := j+1;
               End;
           End;
-					
       End;
   Until j=n;
 End;
